09cfc0d3864b49053b9feb29a1cb023d7a09ffa2
Kenny Chow <kenny@chow.my>
2023-09-23 17:41:37 +0800

    Minor fix

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -1,14 +1,16 @@
+import Link from 'next/link';
+
 const Footer = () => {
   return (
     <div className="w-full">
       <div className="relative flex flex-col md:mx-auto md:w-3/4">
         <footer className="flex flex-col items-center justify-center py-5 space-x-0 space-y-3 border-b md:flex-row md:space-y-0 md:space-x-3">
-          <a
+          <Link
             className="px-5 py-2 text-xl rounded hover:underline"
             href="/support"
           >
             Support
-          </a>
+          </Link>
         </footer>
         <p className="py-5 text-center text-gray-400">
           &copy; Lingui.me. All rights reserved {new Date().getFullYear()}.
